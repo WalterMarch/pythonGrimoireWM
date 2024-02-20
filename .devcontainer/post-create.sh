30bcafea45fcefb21ca2f8dbcd8092aceeeac72a
Walter March <38635451+WalterMarch@users.noreply.github.com>
2024-02-20 18:16:02 -0500

    Update post-create.sh
    adding more python stuff

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -2,7 +2,13 @@
 
 source $1/.devcontainer/configit.sh $1
 
-sudo apt-get update -y
+# Linux utils
+sudo apt-get update
+sudo apt-get install -y tree
 
 sudo apt-get install tk -y
-
+sudo apt-get install -y python3-pip
+pip3 install -U --user --upgrade pip
+pip3 install mypy -U --user
+pip3 install jupyter -U --user --force-reinstall 
+pip3 install ipykernel -U --user --force-reinstall
